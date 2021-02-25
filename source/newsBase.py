750ed4ac374059d2d9e574ac7f628844f13461ed
Aufa Nabil <aufa.nabil.amiri@gmail.com>
2021-02-25 21:02:37 +0700

    inital commit

---
+++ source/newsBase.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class NewsBaseSrc(ABC):
+
+    @abstractmethod
+    def download_url(self, url):
+        pass
+
+    @abstractmethod
+    def get_linked_urls(self, url, html):
+        pass
+
+    @abstractmethod
+    def get_content(self, url):
+        pass
+
+    @abstractmethod
+    def run(self, target_total):
+        pass
