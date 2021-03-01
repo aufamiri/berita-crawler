85b1ebb4a88e2fb8718a3f1df7d73c3aaf8a6008
Aufa Nabil <aufa.nabil.amiri@gmail.com>
2021-03-01 12:28:39 +0700

    add : liputan6(wip) and refactor the baseclass

---
+++ source/liputan6.py
@@ -0,0 +1,61 @@
+from urllib.parse import urljoin
+import requests
+import datetime
+
+from .newsBase import NewsBaseSrc
+
+
+class Liputan6(NewsBaseSrc):
+    def parse_url(self, url, date, page):
+        return url + "/" + \
+            date.strftime("%Y/%m/%d") + "?page=" + str(page)
+
+    def get_default_url(self):
+        return "https://www.liputan6.com/indeks"
+    """
+    <article ... data-type="Article">
+        ...
+        <h4>
+            <a href=...>
+        </h4>
+        ...
+    </article>
+    """
+
+    def get_linked_urls(self, url):
+        html = self.download_url(url)
+        soup = self.make_soup(html)
+        for link in soup.find_all("article", attrs={"data-type": "Article"}):
+
+            for item in link.find_all("h4"):
+
+                path = item.find("a").get('href')
+
+                if path and path.startswith('/'):
+                    path = urljoin(url, path)
+                yield path
+
+    def get_content(self, url):
+        html = self.download_url(url)
+        soup = self.make_soup(html)
+        source = soup.find_all(class_="itp_bodycontent")
+
+        print(f'download URL : {url}')
+
+        result_text = ""
+
+        for text in source[0].find_all("p"):
+
+            # skpping on editorial notes and video promote
+            if (text.find("strong")):
+                continue
+
+            result_text = result_text + text.get_text()
+
+        return result_text
+
+
+if __name__ == '__main__':
+    result = Liputan6().run(end_date=datetime.datetime(2021, 2, 27))
+
+    print(result)
--- source/newsBase.py
+++ source/newsBase.py
@@ -1,20 +1,78 @@
 from abc import ABC, abstractmethod
+import requests
+import datetime
+from bs4 import BeautifulSoup
 
 
 class NewsBaseSrc(ABC):
-
+    # get all the article links from indeks
     @abstractmethod
-    def download_url(self, url):
+    def get_linked_urls(self, url):
         pass
 
-    @abstractmethod
-    def get_linked_urls(self, url, html):
-        pass
-
+    # get the article content and return it as string
     @abstractmethod
     def get_content(self, url):
         pass
 
+    # return the indeks url
     @abstractmethod
-    def run(self, url="", target_total=100, initial_run=1, start_date=None, end_date=None):
+    def parse_url(self, url, date, page):
         pass
+
+    # get default url
+    @abstractmethod
+    def get_default_url(self):
+        pass
+
+    # return bs4 objects,
+    # if the need to change the parser arise,
+    # we just need to change this
+    def make_soup(self, html):
+        return BeautifulSoup(html, "html.parser")
+
+    # request the page and return it as strings
+    def download_url(self, url):
+        return requests.get(url).text
+
+    def run(self, site_url=None, target_total=100, initial_run=1, start_date=datetime.datetime.now(), end_date=datetime.datetime.now()):
+        page = initial_run
+        date = start_date
+
+        prev_download = 0
+        urls_to_download = []
+        result_text_array = []
+
+        if (site_url is None):
+            site_url = self.get_default_url()
+
+        # Search for article url until target is fullfilled
+        # OR
+        # {date} > {end_date}
+        while target_total >= len(urls_to_download):
+            print(len(urls_to_download))
+
+            url = self.parse_url(site_url, date, page)
+            print(f'Crawling: {url}')
+
+            added_data = list(self.get_linked_urls(url))
+
+            urls_to_download.extend(added_data)
+            page += 1
+
+            # change day if no more news is found
+            if (len(added_data) == 0):
+                # break on {date} passing the dateRange
+                if(date >= end_date):
+                    break
+
+                date = date + datetime.timedelta(1)
+                page = 1
+
+        # Download and Parse from URL
+        print(f'Found Total Link: {len(urls_to_download)}')
+        for link in urls_to_download:
+            result = self.get_content(link)
+            result_text_array.append(result)
+
+        return result_text_array
